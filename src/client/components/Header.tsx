17c02b842028ad1d4e2f844ef16c1fc698e51f3c
wildan bimantoro <wildan.bimantoro@ptgue.com>
2022-08-30 22:09:27 +0700

    Add layout home, login & register

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -1,21 +1,37 @@
 /* eslint-disable @next/next/no-img-element */
+import { useRouter } from 'next/router';
 
 function Header() {
+  const router = useRouter();
+
   return (
-    <header>
-      <div className="flex items-center space-x-2 md:space-x-10">
+    <header className="bg-white">
+      <div className="flex bg-white items-center space-x-2 md:space-x-10">
         <img
-          src="/vercel.svg"
+          src="/medex-logo-1.svg"
           width={100}
           height={100}
           className="cursor-pointer object-contain"
         />
         <ul className="hidden space-x-4 md:flex">
-          <li className="customClass">Items 1</li>
-          <li className="customClass">Items 2</li>
-          <li className="customClass">Items 3</li>
-          <li className="customClass">Items 4</li>
-          <li className="customClass">Items 5</li>
+          <li className="customClass">
+            <a href="#home">Beranda</a>
+          </li>
+          <li className="customClass">
+            <a href="#about">Tentang Kami</a>
+          </li>
+          <li className="customClass">Fitur Dan Layanan</li>
+          <li className="customClass">FAQ</li>
+        </ul>
+      </div>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <ul className="hidden space-x-4 md:flex">
+          <li className="customClass" onClick={() => router.push('/login')}>
+            Login
+          </li>
+          <li className="customClass" onClick={() => router.push('/register')}>
+            Register
+          </li>
         </ul>
       </div>
     </header>
--- src/client/pages/register.tsx
+++ src/client/pages/register.tsx
@@ -34,15 +34,16 @@ const Login: FC<ILoginProps> = (props) => {
           <div className="mt-4">
             <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
               <div className="text-2xl text-blue-900 font-semibold">
-                Masuk untuk melanjutkan ke Med-ex!
+                Yuk, daftar sekarang buat dapetin diskon spesial di website
+                Med-ex!
               </div>
               <div className="mt-2 flex justify-center">
-                Belum punya akun?{' '}
+                Sudah punya akun?{' '}
                 <div
                   className="pl-2 text-blue-900 font-extrabold cursor-pointer"
-                  onClick={() => router.push('/register')}
+                  onClick={() => router.push('/login')}
                 >
-                  Daftar
+                  Masuk
                 </div>
               </div>
               <div className="mt-8 mb-4">
@@ -57,6 +58,20 @@ const Login: FC<ILoginProps> = (props) => {
                   id="username"
                   type="text"
                   placeholder="Username"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="text-left block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="text"
+                  placeholder="Email"
                 />
               </div>
               <div className="mb-6">
@@ -77,19 +92,13 @@ const Login: FC<ILoginProps> = (props) => {
                   Please choose a password.
                 </p> */}
               </div>
-              <div className="flex items-center justify-between">
+              <div className="flex items-center justify-center">
                 <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-10 rounded focus:outline-none focus:shadow-outline"
                   type="button"
                 >
-                  Masuk
+                  Daftar
                 </button>
-                <a
-                  className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                  href="#"
-                >
-                  Forgot Password?
-                </a>
               </div>
             </form>
           </div>
