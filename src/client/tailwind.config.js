3c5e727387bc20b77661934fecbf3fe2cadf06eb
Arief Rachman <arief.rachman@goapotik.com>
2022-09-29 20:42:23 +0700

    fix daisyui not load

--- src/client/tailwind.config.js
+++ src/client/tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
   content: [
     path.join(__dirname, './pages/**/*.{js,ts,jsx,tsx}'),
     path.join(__dirname, './components/**/*.{js,ts,jsx,tsx}'),
-    'node_modules/daisyui/dist/**/*.js',
-    'node_modules/react-daisyui/dist/**/*.js',
+    path.join(__dirname, '../../../node_modules/react-daisyui/dist/react-daisyui.cjs'),
+    path.join(__dirname, '../../../node_modules/daisyui/dist/**/*'),
   ],
   theme: {},
   plugins: [require('daisyui')],
